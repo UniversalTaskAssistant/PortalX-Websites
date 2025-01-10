5d70741ddddcbbfb85650a3c5921e61244efbbc6
MulongXie <dsh15325@163.com>
2025-01-10 16:44:16 +1100

    revise deploy config to 7000

--- homepage/vite.config.js
+++ homepage/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 7000,
+    host: true // This allows external access to the dev server
+  }
 })
